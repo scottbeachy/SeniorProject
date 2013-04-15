4e3febd87d908e0905be804af603014ca5e5bed0
Paul Ortega <paulortega@gmail.com>
2013-04-15 00:12:26 -0700

    iTextSharp dll & PDF test added
    Added "PDFs" folder & pdfTest.aspx. Open up pdfTest and a pdf with
    sample text will be created in the PDFs folder.
    ...Also, the contact page was giving me an error during debugging
    because there wasn't a contact code behind file. I went ahead and
    created a new (empty) contact page.

---
+++ WeServeU/pdfTest.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//add itextsharp using references
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+public partial class pdfTest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //instantiate new pdf doc
+        Document doc1 = new Document();
+
+        //create the pdf in the "PDFs" folder
+        string path = Server.MapPath("PDFs");
+        PdfWriter.GetInstance(doc1, new FileStream(path + "/doc1.pdf", FileMode.Create));
+
+        //edit the newly created pdf
+        doc1.Open();
+        doc1.Add(new Paragraph("My first PDF"));
+        doc1.Close();
+    }
+}
