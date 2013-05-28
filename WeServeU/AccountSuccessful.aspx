4884443a9e94820d1b41e3b9505c395aa5ca7ff0
Paul Ortega <paulortega@gmail.com>
2013-05-27 19:54:36 -0700

    Added some validation & redirect page

---
+++ WeServeU/AccountSuccessful.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="AccountSuccessful.aspx.cs" Inherits="AccountSuccessful" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <p id="redirectText">Your Account has been successfully created. Please wait while you are redirected to the login page...or click <a href="login.aspx">here</a> to be redirected.</p>
+</asp:Content>
+
--- WeServeU/requestServices.aspx.cs
+++ WeServeU/requestServices.aspx.cs
@@ -17,36 +17,50 @@ public partial class requestServices : System.Web.UI.Page
     }
     protected void btnCreateAcct_Click(object sender, EventArgs e)
     {
-        //Create parameterized insert statement
-        string insertStmt = "INSERT into Customer(CFname, CLname, CFirmName, CPhone, CFax, " +
-        "CEmail, CAddress, CApt, CCity, CState, CCounty, CZip, CUser, CPass, CPerm) " +
-        "VALUES(@CFname, @CLname, @CFirmName, @CPhone, @CFax, @CEmail, @CAddress, @CApt, @CCity, @CState, @CCounty, @CZip, @CUser, @CPass, @CPerm)";
+        try
+        {
+            //Create parameterized insert statement
+            string insertStmt = "INSERT into Customer(CFname, CLname, CFirmName, CPhone, CFax, " +
+            "CEmail, CAddress, CApt, CCity, CState, CCounty, CZip, CUser, CPass, CPerm) " +
+            "VALUES(@CFname, @CLname, @CFirmName, @CPhone, @CFax, @CEmail, @CAddress, @CApt, @CCity, @CState, @CCounty, @CZip, @CUser, @CPass, @CPerm)";
 
-        //Set up connection
-        string connection = ConfigurationManager.ConnectionStrings["testDB"].ConnectionString;
-        SqlConnection conn = new SqlConnection(connection);
+            //Set up connection
+            string connection = ConfigurationManager.ConnectionStrings["testDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connection);
 
-        //Iniatalize & add parameters to the SqlCommand object
-        SqlCommand cmd = new SqlCommand(insertStmt, conn);
-        cmd.Parameters.AddWithValue("@CFname", txtFname.Text);
-        cmd.Parameters.AddWithValue("@CLname", txtLname.Text);
-        cmd.Parameters.AddWithValue("@CFirmName", txtFirmName.Text);
-        cmd.Parameters.AddWithValue("@CPhone", txtPhone.Text);
-        cmd.Parameters.AddWithValue("@CFax", txtFax.Text);
-        cmd.Parameters.AddWithValue("@CEmail", txtEmail.Text);
-        cmd.Parameters.AddWithValue("@CAddress", txtAddress.Text);
-        cmd.Parameters.AddWithValue("@CApt", txtApartment.Text);
-        cmd.Parameters.AddWithValue("@CCity", txtCity.Text);
-        cmd.Parameters.AddWithValue("@CState", ddState.SelectedValue);
-        cmd.Parameters.AddWithValue("@CCounty", txtCounty.Text);
-        cmd.Parameters.AddWithValue("@CZip", txtZip.Text);
-        cmd.Parameters.AddWithValue("@CUser", txtUsername.Text);
-        cmd.Parameters.AddWithValue("@CPass", txtPassword.Text);
-        cmd.Parameters.AddWithValue("@CPerm", "C");
+            //Iniatalize & add parameters to the SqlCommand object
+            SqlCommand cmd = new SqlCommand(insertStmt, conn);
+            cmd.Parameters.AddWithValue("@CFname", txtFname.Text);
+            cmd.Parameters.AddWithValue("@CLname", txtLname.Text);
+            cmd.Parameters.AddWithValue("@CFirmName", txtFirmName.Text);
+            cmd.Parameters.AddWithValue("@CPhone", txtPhone.Text);
+            cmd.Parameters.AddWithValue("@CFax", txtFax.Text);
+            cmd.Parameters.AddWithValue("@CEmail", txtEmail.Text);
+            cmd.Parameters.AddWithValue("@CAddress", txtAddress.Text);
+            cmd.Parameters.AddWithValue("@CApt", txtApartment.Text);
+            cmd.Parameters.AddWithValue("@CCity", txtCity.Text);
+            cmd.Parameters.AddWithValue("@CState", ddState.SelectedValue);
+            cmd.Parameters.AddWithValue("@CCounty", txtCounty.Text);
+            cmd.Parameters.AddWithValue("@CZip", txtZip.Text);
+            cmd.Parameters.AddWithValue("@CUser", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@CPass", txtPassword.Text);
+            cmd.Parameters.AddWithValue("@CPerm", "C");
 
-        //Open connection, execute SqlCommand, & close connection
-        conn.Open();
-        cmd.ExecuteNonQuery();
-        conn.Close();
+            //Open connection, execute SqlCommand, & close connection
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            //Send to account successful page
+            Response.Redirect("AccountSuccessful.aspx");
+        }
+
+        catch(SqlException ex)
+        {
+            lblError.Text = "Account could not be created. Please try again later.";
+            lblError.Visible = true;
+        }
+
+
     }
 }
