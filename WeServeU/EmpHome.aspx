a036bf603c053c4d14ead98bcff91bc650b94968
Josh Snyder <bigjaash@yahoo.com>
2013-05-28 16:11:54 -0700

    EmpHome Changes
    Github went ballistic on me again...

--- WeServeU/EmpHome.aspx
+++ WeServeU/EmpHome.aspx
@@ -3,8 +3,55 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+
+    <script type="text/javascript">
+
+        $(document).ready(function () {
+            $(".txtDate").datepicker();
+        });
+    </script>
+
     <div class="welcomeBox">
         <asp:Label ID="lblWelcome" runat="server" Text=""></asp:Label>
+    </div>
+
+
+    <div class="adminDashboard">
+        <h2>Please select your task below</h2>
+        <h3>WORK ORDERS</h3>
+        <div id="empSelect">
+            
+            <div id="woSelect">
+                <p>Select a Work Order From Below To View:</p>
+                
+                <asp:DropDownList ID="ddlWOList" runat="server" DataSourceID="SelectWODDL" DataTextField="Parties"  DataValueField="WorkOrderID" OnSelectedIndexChanged="ddlWOList_SelectedIndexChanged" AutoPostBack="true">                
+                </asp:DropDownList><br />
+                <asp:SqlDataSource ID="SelectWODDL" runat="server" ConnectionString="<%$ ConnectionStrings:UpdateWODataConnection %>" SelectCommand="SELECT [WorkOrderID],  [WLName] + ' VS ' + [WOPLName] AS Parties FROM [WorkOrder] WHERE ([WStatus] = @WStatus) ORDER BY [WorkOrderID]">
+                    <SelectParameters>
+                        <asp:Parameter DefaultValue="A" Name="WStatus" Type="String" />
+                    </SelectParameters>
+                </asp:SqlDataSource>
+                <asp:Label ID="lblWONum" runat="server" Text="Work Order #" Visible="false"></asp:Label><br />
+                
+                <asp:Button ID="btnAssign" runat="server" Text="View Work Order" CssClass="btnDash" OnClick="btnAssign_Click" /><br />
+                <asp:Label ID="lblAssignError" runat="server" Text="" ForeColor="Red" Visible="false"></asp:Label>
+            </div>
+        </div>
+        
+        <h3>To Update a Workorder, Please Select a Date Range Below:</h3>
+        <asp:Label ID="lblWrkDate" runat="server" Text="Begining Date:" CssClass="lblDash"></asp:Label>
+        <asp:TextBox ID="txtWrkDate1" runat="server" ValidationGroup="wrkUpdate" CssClass="txtDate" ViewStateMode="Enabled"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="valDate" runat="server" ErrorMessage="*" ForeColor="Red" ValidationGroup="wrkUpdate" ControlToValidate="txtWrkDate1"></asp:RequiredFieldValidator>
+        <asp:Label ID="lblWrkDate1" runat="server" Text="End Date:" CssClass="lblDash"></asp:Label>
+        <asp:TextBox ID="txtWrkDate2" runat="server" ValidationGroup="wrkUpdate" CssClass="txtDate" ViewStateMode="Enabled"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="valDate1" runat="server" ErrorMessage="*" ForeColor="Red" ValidationGroup="wrkUpdate" ControlToValidate="txtWrkDate2"></asp:RequiredFieldValidator>
+        <asp:Button ID="btnUpdateWrk" runat="server" Text="Submit" CssClass="btnDash" ValidationGroup="wrkUpdate" CausesValidation="true" OnClick="btnUpdateWrk_Click" /><br />
+        <asp:DropDownList ID="ddlUpdateWO" runat="server" Visible="false" CssClass="btnDash"></asp:DropDownList><asp:Button ID="btnChoose" runat="server" Text="Update" OnClick="btnChoose_Click"  CssClass="btnDash" /><br />
+        <asp:Label ID="lblUpdate" runat="server" Text="" ForeColor="Red" Visible="false"></asp:Label>
+
+        <br />
+        <asp:Button ID="btnLogout" runat="server" Text="Log Out" CssClass="btnDash" OnClick="btnLogout_Click" />
+
     </div>
 </asp:Content>
 
--- WeServeU/EmpHome.aspx.cs
+++ WeServeU/EmpHome.aspx.cs
@@ -1,6 +1,6 @@
 ﻿/*This is the logged in Employee Home Page
- * Created by Scott Beachy
- * 4/30/13
+ * Created by Josh Snyder
+ * 5/24/13
  * */
 
 using System;
@@ -11,6 +11,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Net;
 
 public partial class EmpHome : System.Web.UI.Page
 {
@@ -19,6 +22,8 @@ public partial class EmpHome : System.Web.UI.Page
     string fname;
     string lname;
     string perm;
+    string date1;
+    string date2;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -42,5 +47,107 @@ public partial class EmpHome : System.Web.UI.Page
 
         //Custom welcome message on the screen
         lblWelcome.Text = "Welcome " + fname + " " + lname + ". Employee ID: " + id;
+    }
+
+    protected void btnUpdateWrk_Click(object sender, EventArgs e)
+    {
+        date1 = txtWrkDate1.Text;
+        date2 = txtWrkDate2.Text;
+        try
+        {
+            //Connnect to DB and fill the ddl list with the WorkOrder Values
+            string connection = ConfigurationManager.ConnectionStrings["testDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("Select WorkOrder.WorkOrderID, WorkOrder.WLName + ' VS ' + WorkOrder.WOPLName AS CustomerName FROM WorkOrder WHERE WorkOrder.WDateCreated Between @date1 AND @date2 ORDER BY WorkOrderID;", conn);
+            cmd.Parameters.AddWithValue("date1", date1);
+            cmd.Parameters.AddWithValue("date2", date2);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count > 0)
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+
+                    ddlUpdateWO.Items.Add(new ListItem(dt.Rows[i][0].ToString() + " " + dt.Rows[i][1].ToString(), dt.Rows[i][0].ToString()));
+
+                }
+            }
+
+            else
+            {
+                //If there are no workorders in the specified date range, display this message
+                lblUpdate.Text = ("There are no work orders in that date range. Please select another range.");
+                lblUpdate.Visible = true;
+            }
+            //make both the select button and the ddl visible
+            btnChoose.Visible = true;
+            ddlUpdateWO.Visible = true;
+            conn.Close();
+        }
+        catch (Exception ex)
+        {
+            //Oops, something bad happened...
+            lblUpdate.Text = ("There was an error connecting to the database. Description: " + ex);
+            lblUpdate.Visible = true;
+        }
+
+    }
+
+    protected void btnChoose_Click(object sender, EventArgs e)
+    {
+        string id = null;
+
+        id = ddlUpdateWO.SelectedValue;
+
+        Session["WorkOrderID"] = id;
+
+        Response.Redirect("UpdateWorkOrder.aspx");
+    }
+
+
+    protected void ddlWOList_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        lblWONum.Text = "Work Order # " + ddlWOList.SelectedValue.ToString();
+        lblWONum.Visible = true;
+    }
+
+
+    protected void btnAssign_Click(object sender, EventArgs e)
+    {
+        string workOrderNum;
+        //string empNum;
+        workOrderNum = ddlWOList.SelectedValue;
+        //empNum = ddlEmpList.SelectedValue;
+        try
+        {
+            string connection = ConfigurationManager.ConnectionStrings["testDB"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("UPDATE WorkOrder SET EmpID = @EmpID WHERE WorkOrderID = @WorkOrderID;", conn);
+            //cmd.Parameters.AddWithValue("@EmpID", empNum);
+            cmd.Parameters.AddWithValue("@WorkOrderID", workOrderNum);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            //lblWONum.Text = ddlEmpList.SelectedItem.ToString() + " was assigned to work order #" + workOrderNum;
+            lblWONum.Visible = true;
+
+        }
+        catch (SqlException ex)
+        {
+            lblWONum.Text = "The work order was not assigned due to an error";
+            lblWONum.ForeColor = System.Drawing.Color.Red;
+            lblWONum.Visible = true;
+        }
+
+
+
+    }
+
+    protected void btnLogout_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("login.aspx");
     }
 }
